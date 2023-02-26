d420894bcc309b6cf62889a7af8f2e26630f4d6a
kouseinadaffa <kouseinadaffa@gmail.com>
2023-02-26 15:05:21 +0700

    first init

---
+++ node/app.js
@@ -0,0 +1,11 @@
+const createReadlineInterface = require('./lib/readline-interface');
+
+console.log('Calculator CLI');
+console.log('');
+createReadlineInterface(main);
+
+// Calculator App
+
+function main(input) {
+  // Your Code Here
+}
